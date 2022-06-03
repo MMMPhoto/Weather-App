49c06c5fdce1b8653d9f80a1372f076026476e37
Max McDonough <max.mcdonough@gmail.com>
2022-06-03 11:47:28 -0400

    Test city list import (v1.3)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,12 @@ let inputForm = document.createElement('form');
 let inputBox = document.createElement('input');
 let submitButton = document.createElement('button');
 
+fetch('./assets/js/current.city.list.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(jsondata => console.log(jsondata));
+
 inputBox.setAttribute('type', 'text');
 submitButton.innerHTML = 'Submit';
 
@@ -14,10 +20,10 @@ let apiKey = '3a8edcb6a734dc2c076a743098ed3084';
 let searchQuery = 'Columbus,GA,USA';
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&APPID=${apiKey}`)
-  .then(response => {
-    console.log(`fetch status: code ${response.status}`);
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+    .then(response => {
+        console.log(`fetch status: code ${response.status}`);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    });
