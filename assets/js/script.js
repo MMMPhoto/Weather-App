16196711d3109bcf57bf6635d8273ae7e7dc8c9d
Max McDonough <max.mcdonough@gmail.com>
2022-06-03 20:31:38 -0400

    Search query and API fetch working (v1.6)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,18 @@ let body = document.body;
 let inputForm = document.createElement('form');
 let inputBox = document.createElement('input');
 let submitButton = document.createElement('button');
-let testArray = ['Atlanta', 'Atlantis', 'Atalanta'];
 let rawCityList = [];
 let easyCityList = [];
 let city = {};
+let apiKey = '3a8edcb6a734dc2c076a743098ed3084';
+let searchQuery;
+let weatherData;
+
+
+
+// let jsondata = require('./assets/js/city.list.json');
+// console.log(jsondata);
+
 
 // Pull City List
 fetch('./assets/js/city.list.json')
@@ -35,20 +43,33 @@ body.append(inputForm);
 inputForm.append(inputBox);
 inputForm.append(submitButton);
 
-let apiKey = '3a8edcb6a734dc2c076a743098ed3084';
-let searchQuery = 'Columbus,GA,USA';
+$(inputBox).autocomplete( {
+    appendTo: inputForm,
+    minLength: 3,
+    source: easyCityList
+});
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&APPID=${apiKey}`)
+let weatherFetch = (searchQuery) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&APPID=${apiKey}`)
     .then(response => {
         console.log(`fetch status: code ${response.status}`);
         return response.json();
     })
     .then(data => {
         console.log(data);
+        weatherData = data;
+        weatherDisplay(weatherData);   
     });
+};
 
-$(inputBox).autocomplete( {
-    appendTo: inputForm,
-    minLength: 3,
-    source: easyCityList
+let weatherDisplay = (weatherData) => {
+    let weatherContainer = document.createElement('div');
+    weatherContainer.innerHTML = `${weatherData.name}<br>Temp: ${weatherData.main.temp}<br>Wind speed: ${weatherData.wind.speed}<br>Humidity: ${weatherData.main.humidity}%<br>`;
+    body.append(weatherContainer);
+}
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    searchQuery = inputBox.value;
+    weatherFetch(searchQuery);
 });
